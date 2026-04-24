f6552919466363d2fb0db76cc7ed833662bf2001
Nakshatra Garg <nakshatra@kugelblitz.xyz>
2026-04-24 11:59:44 +0530

    project setup completed, initial commit

---
+++ src/main/java/com/nakshatra/backup_saas/BackupSaasApplication.java
@@ -0,0 +1,13 @@
+package com.nakshatra.backup_saas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackupSaasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackupSaasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nakshatra/backup_saas/BackupSaasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nakshatra.backup_saas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackupSaasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
